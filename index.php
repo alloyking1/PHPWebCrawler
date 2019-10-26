2d5d939d7e844921c5766cd97dcbc5c7b20c307b
alloyking1 <alloyking1@gmail.com>
2019-10-26 10:20:57 -0400

    image saving to file now

--- index.php
+++ index.php
@@ -6,35 +6,25 @@
 
     // Find all images
     foreach($url->find('img') as $element){
-        
-        // $file_name = $element->src;
-        // $filepath2 = $_GET[$file_name];
 
+        //save img using cUrl
+        $start = curl_init();
+        curl_setopt($start, CURLOPT_URL, $element->src);
+        curl_setopt($start, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($start, CURLOPT_SSLVERSION, 3);
+        $file_data = curl_exec($start);
+        curl_close($start);
 
-        // $filepath = '/img'.$file_name2; 
-        // header('Cache-Control: Public');
-        // header('Content-Description: File Transfer');
-        // header('Content-Disposition: attachment; filename="'.$file_name.'"');
-        // header('Content-Type: application/zip');
-        // header('Content-Transfer-Encoding: binary');
-
-        // readfile($filepath); //Absolute URL
+        $file_path = 'img/'. uniqid(). '.jpg';
+        // writing file to memory
+        $file = fopen($file_path, 'w+');
+        fputs($file, $file_data);
+        fclose($file);
 
         // echo $element->src. '<img src="{{$element->src}}"/>';
         echo '<img src="' . $element->src . '" alt="error">'; 
+        // echo '<img src="' . $file_path. '" alt="error">'; 
     }
-    
-    
-    // $uploads_dir = '/img';
-    // foreach ($url->find('img') as $element) {
-    //     $tmp_name = $_FILES[$element->src]["tmp_name"][$key];
-    //     // basename() may prevent filesystem traversal attacks;
-    //     // further validation/sanitation of the filename may be appropriate
-    //     $name = basename($_FILES[$element->src]["name"][$key]);
-    //     move_uploaded_file($tmp_name, "$uploads_dir/$name");
-        
-    //     echo $element->src ."File downloaded!". '<br>';
-    // }
 
     // Find all links
     foreach($url->find('a') as $element){
@@ -43,6 +33,4 @@
     }
 ?>
 
-
-<img src="https://kolorsplash.com/wp-content/uploads/2019/09/H34e7947d36e04cafbba7d82445615d67n-262x262.jpg"/>
         
