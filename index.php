34569d672bae918898ebefae73321606b12d997d
alloyking1 <alloyking1@gmail.com>
2019-10-26 09:39:13 -0400

    image display from crowler not saving yet

--- index.php
+++ index.php
@@ -6,41 +6,43 @@
 
     // Find all images
     foreach($url->find('img') as $element){
+        
+        // $file_name = $element->src;
+        // $filepath2 = $_GET[$file_name];
 
-        $file_url = $element->src;
-        $file_name=0147; 
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.$file_name.'"');
-        header('Content-Transfer-Encoding: binary');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($file_url)); //Absolute URL
-        ob_clean();
-        flush();
-        readfile($file_url); //Absolute URL
 
-        echo $element->src ."File downloaded!". '<br>';
-    }   
+        // $filepath = '/img'.$file_name2; 
+        // header('Cache-Control: Public');
+        // header('Content-Description: File Transfer');
+        // header('Content-Disposition: attachment; filename="'.$file_name.'"');
+        // header('Content-Type: application/zip');
+        // header('Content-Transfer-Encoding: binary');
+
+        // readfile($filepath); //Absolute URL
+
+        // echo $element->src. '<img src="{{$element->src}}"/>';
+        echo '<img src="' . $element->src . '" alt="error">'; 
+    }
+    
+    
+    // $uploads_dir = '/img';
+    // foreach ($url->find('img') as $element) {
+    //     $tmp_name = $_FILES[$element->src]["tmp_name"][$key];
+    //     // basename() may prevent filesystem traversal attacks;
+    //     // further validation/sanitation of the filename may be appropriate
+    //     $name = basename($_FILES[$element->src]["name"][$key]);
+    //     move_uploaded_file($tmp_name, "$uploads_dir/$name");
+        
+    //     echo $element->src ."File downloaded!". '<br>';
+    // }
 
     // Find all links
     foreach($url->find('a') as $element){
         echo $element->href . '<br>';
 
     }
+?>
 
 
-    function file_get_contents_curl($url) { 
-        $ch = curl_init(); 
-        
-        curl_setopt($ch, CURLOPT_HEADER, 0); 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-        curl_setopt($ch, CURLOPT_URL, $url); 
-        
-        $data = curl_exec($ch); 
-        curl_close($ch); 
-        
-        return $data; 
-    } 
+<img src="https://kolorsplash.com/wp-content/uploads/2019/09/H34e7947d36e04cafbba7d82445615d67n-262x262.jpg"/>
         
