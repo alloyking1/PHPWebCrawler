cdd008870ea27d247b6f43836da1870bf011fe11
alloyking1 <alloyking1@gmail.com>
2019-10-26 11:06:34 -0400

    image download now working

--- index.php
+++ index.php
@@ -7,19 +7,19 @@
     // Find all images
     foreach($url->find('img') as $element){
 
-        //save img using cUrl
-        $start = curl_init();
-        curl_setopt($start, CURLOPT_URL, $element->src);
-        curl_setopt($start, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($start, CURLOPT_SSLVERSION, 3);
-        $file_data = curl_exec($start);
-        curl_close($start);
+        // save image to file
+        $ch = curl_init($element->src);
+        $my_save_dir = 'img/';
+        $filename = basename($element->src);
+        $complete_save_loc = $my_save_dir . $filename;
 
-        $file_path = 'img/'. uniqid(). '.jpg';
-        // writing file to memory
-        $file = fopen($file_path, 'w+');
-        fputs($file, $file_data);
-        fclose($file);
+        $fp = fopen($complete_save_loc, 'wb');
+
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fp);
 
         // echo $element->src. '<img src="{{$element->src}}"/>';
         echo '<img src="' . $element->src . '" alt="error">'; 
